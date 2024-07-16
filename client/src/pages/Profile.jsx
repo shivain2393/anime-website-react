5a078c099bc87871d77d2e57e328fbc8ca08faae
Shivain Sagar <shivain.sagar@gmail.com>
2024-07-16 19:29:46 +0530

    Improved Sign up page UI

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import WatchList from '../components/WatchList';
 import { updateUserDetailsStart, updateUserDetailsSuccess, updateUserDetailsFailure } from '../redux/user/userSlice.js'
@@ -14,6 +14,8 @@ const Profile = () => {
   const [disableSave, setDisableSave] = useState(true);
   const dispatch = useDispatch();
 
+  const avatarImageRef = useRef(null);
+  const backgroundImageRef = useRef(null);
 
   useEffect(() => {
     if(avatarImage || backgroundImage){
@@ -112,24 +114,35 @@ const Profile = () => {
     <div className='profile'>
       <div className="profile-info" style={backgroundImageStyle}>
         <div className="profile-details">
-          <img src={currentUser.avatar} width={200} alt="profile-icon" className="profile-icon" />
+          <div className='profile-icon-container'>
+            <img src={currentUser.avatar} width={200} alt="profile-icon" className="profile-icon" />
+            {editMode && <div onClick={() => avatarImageRef.current.click()} className="profile-pen">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" width={30}>
+                <path strokeLinecap="round" strokeLinejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125" />
+              </svg>
+            </div>}
+          </div>
           <span>{currentUser.username}</span>
           <span>{currentUser.role}</span>
         </div>
+        {editMode && <div onClick={() => backgroundImageRef.current.click()} className='pen-icon'>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" width={60}>
+            <path strokeLinecap="round" strokeLinejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125" />
+          </svg>
+        </div>}
         <div className="edit-profile">
           {!editMode && 
           <button onClick={handleEditMode} type='button' className='animated-btn'>Edit</button>}
           {editMode && 
           <>
-            <input onChange={(e) => setAvatarImage(e.target.files[0])} accept='image/*' type="file" name="" id="avatar" />
-            <input onChange={(e) => setBackgroundImage(e.target.files[0])} accept='image/*' type="file" name="" id="backgroundImage" />
+            <input style={{display: 'none'}}  ref={avatarImageRef} onChange={(e) => setAvatarImage(e.target.files[0])} accept='image/*' type="file" name="" id="avatar" />
+            <input style={{display: 'none'}} ref={backgroundImageRef} onChange={(e) => setBackgroundImage(e.target.files[0])} accept='image/*' type="file" name="" id="backgroundImage" />
             <button disabled={disableSave} onClick={handleSubmit} className='animated-btn' >{loading ? 'Loading...' : 'Save'}</button>
             <button disabled={loading} onClick={handleEditMode} type='button' className='animated-btn' >Cancel</button> 
           </>
           }
         </div>
       </div>
-      
       <div className="personal-anime-info">
           <WatchList />
       </div>
