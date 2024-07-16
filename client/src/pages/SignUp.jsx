426395450edbadbdc8f0a5aab9f9eeaef173aca8
Shivain Sagar <shivain.sagar@gmail.com>
2024-07-16 20:22:43 +0530

    added eye icon for password fields

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -6,6 +6,8 @@ const SignUp = () => {
   const [formData, setFormData] = useState({});
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+  const [isConfirmPasswordVisible, setIsConfirmPasswordVisible] = useState(false);
 
   const navigate = useNavigate()
 
@@ -68,6 +70,14 @@ const SignUp = () => {
     }
   }
 
+  const handlePasswordVisibility = () => {
+    setIsPasswordVisible(!isPasswordVisible);
+  }
+
+  const handleConfirmPasswordVisibility = () => {
+    setIsConfirmPasswordVisible(!isConfirmPasswordVisible);
+  }
+
   return (
     <div className='sign-up-box'>
       <form className='sign-up-form' onSubmit={(e) => handleSubmit(e)}>
@@ -81,11 +91,33 @@ const SignUp = () => {
         </div>
         <div className='fields'>
           <label htmlFor="password">Password</label>
-          <input onChange={(e) => handleChange(e)} minLength={8} maxLength={64} placeholder='Enter your password' type="password" id='password' required/>
+          <div className="password">
+            <input onChange={(e) => handleChange(e)} minLength={8} maxLength={64} placeholder='Enter your password' type={isPasswordVisible ? "text" : "password"} id='password' required/>
+            {isPasswordVisible ?
+              <svg onClick={handlePasswordVisibility} className='eye-icon' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" width={20}>
+                <path strokeLinecap="round" strokeLinejoin="round" d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88" />
+              </svg> :
+              <svg onClick={handlePasswordVisibility} className='eye-icon' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" width={20}>
+                <path strokeLinecap="round" strokeLinejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
+                <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+              </svg>
+            }
+          </div>
         </div>
         <div className='fields'>
-          <label htElFor="confirmPassword">Confirm Password</label>
-          <input onChange={(e) => handleChange(e)} minLength={8} maxLength={64} placeholder='Confirm your password' type="password" id="confirmPassword" required/>
+          <label htmlFor="confirmPassword">Confirm Password</label>
+          <div className="password">
+            <input onChange={(e) => handleChange(e)} minLength={8} maxLength={64} placeholder='Confirm your password' type={isConfirmPasswordVisible ? "text" : "password"} id="confirmPassword" required/>
+            {isConfirmPasswordVisible ?
+              <svg onClick={handleConfirmPasswordVisibility} className='eye-icon' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" width={20}>
+                <path strokeLinecap="round" strokeLinejoin="round" d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88" />
+              </svg> :
+              <svg onClick={handleConfirmPasswordVisibility} className='eye-icon' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" width={20}>
+                <path strokeLinecap="round" strokeLinejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
+                <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+              </svg>
+            }
+          </div>
         </div>
         {error && <p className='error'>{error}</p>}
         <button disabled={loading} className='submit-btn'>{loading ? 'Loading...' : 'Sign up'}</button>
